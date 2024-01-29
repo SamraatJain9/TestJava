83429d5bbdb8881e321eb8d35bb92ee82e6ca5ad
Samraat-LK789 <samraat1official8@gmail.com>
2024-01-30 01:31:12 +0530

    Trying something diff/easier

---
+++ src/MethodExample.java
@@ -0,0 +1,7 @@
+public class MethodExample {
+
+    public static int calculate(int x, int y){
+        int answer = (x * y) / 2 * 100;
+        return answer;
+    }
+}
---
+++ src/MethodExampleMain.java
@@ -0,0 +1,7 @@
+public class MethodExampleMain extends  MethodExample{
+
+    public static void main(String[] args) {
+        int answer = calculate(5, 10);
+        System.out.println(answer);
+    }
+}
