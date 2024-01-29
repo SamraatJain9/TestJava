24f4d0299f96a19921f823b0fa55b32084878b06
Samraat-LK789 <samraat1official8@gmail.com>
2024-01-29 19:15:03 +0530

    Java Begins

---
+++ src/mark1.java
@@ -0,0 +1,16 @@
+public class mark1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hallo");
+        greet("Sebastian");
+    }
+
+    public static void greet(String name){
+        System.out.println("Hello " + name);
+
+    }
+
+    public mark1() {
+    }
+}
